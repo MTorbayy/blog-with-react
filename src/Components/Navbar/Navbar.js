d4663e6e8fc9dfa30d0e518bfc6a13d36e579833
Marie Torbay <marie.torbay@gmail.com>
2022-01-21 17:14:42 +0100

    first commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import './Navbar.css'
+import Menu from './menu.svg'
+
+export default function Navbar() {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [largeur, setLargeur] = useState(window.innerWidth);
+
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
+    useEffect(() => {
+        
+        const changeWidth = () => {
+            setLargeur(window.innerWidth)
+        }
+        
+        window.addEventListener('resize', changeWidth)
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+
+    }, [])
+
+
+    return (
+        <nav>
+            {(toggleMenu || largeur > 500) && (
+            <ul className="liste">
+                <li className="items">Acceuil</li>
+                <li className="items">Services</li>
+                <li className="items">Contact</li>
+            </ul>
+            )}
+            
+            <img 
+            src={Menu}
+            onClick={toggleNav}
+            className="btn" alt="Menu" />
+        </nav>
+    )
+}
